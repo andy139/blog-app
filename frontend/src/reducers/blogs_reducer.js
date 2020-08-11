de03bab201f7bceec288a5530696d50a14dce7fa
Andy Tran <antr900@gmail.com>
2020-08-11 09:42:41 -0700

    Set up react frontend, blog reducers, and actions

---
+++ frontend/src/reducers/blogs_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_BLOGS, RECEIVE_BLOG, RECEIVE_NEW_BLOG } from '../actions/blog_actions';
+
+const BlogsReducer = (state = { all: {}, blog: {}, new: undefined }, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+    switch (action.type) {
+        case RECEIVE_BLOGS:
+            newState.all = action.blogs.data;
+            return newState;
+        case RECEIVE_BLOG:
+            newState.blog = action.blog.data;
+            return newState;
+        case RECEIVE_NEW_BLOG:
+            newState.new = action.blog.data
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default BlogsReducer;
---
+++ frontend/src/util/blog_api_util.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const getBlogs = () => {
+    return axios.get(`/api/blogs`)
+}
+
+export const getBlogById = id => {
+    return axios.get(`/api/blogs/${id}`)
+};
+
+
+export const createBlog = data => {
+    return axios.post(`/api/blogs`, data)
+}
