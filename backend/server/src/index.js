0666ae5b08a07661efc9df7dec086deb57ddb229
Andy Tran <antr900@gmail.com>
2020-08-12 16:12:20 -0700

    Change route to static build folder

--- backend/server/src/index.js
+++ backend/server/src/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
-    app.get('/', (req, res) => {
+    app.get('/api', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
