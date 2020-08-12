191efca11f08648832e8ff9d3e2748c3bf86a88b
Andy Tran <antr900@gmail.com>
2020-08-12 16:08:23 -0700

    Set up heroku

--- backend/server/src/index.js
+++ backend/server/src/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const routes = require("../../routes");
 const app = express();
+
+const path = require('path')
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 app.use(express.json());
 app.use("/api", routes);
 
