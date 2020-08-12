d11acf6e9c1590455e3a5a7787ffa871b39fb9d6
Andy Tran <antr900@gmail.com>
2020-08-12 16:25:12 -0700

    Set up readme

--- backend/server/src/index.js
+++ backend/server/src/index.js
@@ -4,12 +4,12 @@ const app = express();
 
 const path = require('path')
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'))
-//     app.get('/api', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
+    app.get('/api', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 app.use(express.json());
 app.use("/api", routes);
 
